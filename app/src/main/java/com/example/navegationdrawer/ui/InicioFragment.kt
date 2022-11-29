2c3da77abe58ad488d079200278d545f19eadb9d
Eddieajoy <115562326+Eddieajoy@users.noreply.github.com>
2022-11-28 21:53:19 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/navegationdrawer/ui/InicioFragment.kt
@@ -0,0 +1,13 @@
+package com.example.navegationdrawer.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.navegationdrawer.R
+
+
+class InicioFragment : Fragment(R.layout.fragment_inicio) {
+
+}
